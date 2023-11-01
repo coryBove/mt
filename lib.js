eb8eb668458e8f118ec9cf1d4340e06c04ad5fbf
cb <cory.bove203@gmail.com>
2023-11-01 16:53:08 -0400

    initial commit

---
+++ lib.js
@@ -0,0 +1,524 @@
+// UNDERSTANDING MEDICAL TERMINOLOGY FROM: The MERCK Manual of Medical Information - Second Home Edition
+// Hand-typed from a library copy.
+
+var terms =
+{
+    "a(n)": {
+        ">>>": "absence of"
+    },
+    "acou, acu": {
+        ">>>": "hear"
+    },
+    "aden(o)": {
+        ">>>": "gland"
+    },
+    "aer(o)": {
+        ">>>": "air"
+    },
+    "alg": {
+        ">>>": "pain"
+    },
+    "andr(o)": {
+        ">>>": "man"
+    },
+    "angi(o)": {
+        ">>>": "vessel"
+    },
+    "ankyl(o)": {
+        ">>>": "crooked, curved"
+    },    
+    "ante": {
+        ">>>": "before"
+    },
+    "anter(i)": {
+        ">>>": "front, forward"
+    },
+    "anti": {
+        ">>>": "against"
+    },
+    "arteri(o)": {
+        ">>>": "artery"
+    },
+    "arthr(o)": {
+        ">>>": "joint"
+    },
+    "articul": {
+        ">>>": "joint"
+    },
+    "ather(o)": {
+        ">>>": "fatty"
+    },    
+    "audi(o)": {
+        ">>>": "hearing"
+    },
+    "aur(i)": {
+        ">>>": "ear"
+    },
+    "aut(o)": {
+        ">>>": "self"
+    },
+    "bi, bis": {
+        ">>>": "double, twice, two"
+    },
+    "brachy": {
+        ">>>": "short"
+    },
+    "brady": {
+        ">>>": "slow"
+    },
+    "bucc(o)": {
+        ">>>": "cheek"
+    },
+    "carcin(o)": {
+        ">>>": "cancer"
+    },
+    "cardi(o)": {
+        ">>>": "heart"
+    },
+    "cephal(o)": {
+        ">>>": "head"
+    },
+    "cerebr(o)": {
+        ">>>": "brain"
+    },
+    "cervic": {
+        ">>>": "neck"
+    },
+    "chol(e)": {
+        ">>>": "bile, or referring to gallbladder"
+    },
+    "chondr(o)": {
+        ">>>": "cartilage"
+    },
+    "circum": {
+        ">>>": "around, about"
+    },
+    "contra": {
+        ">>>": "against, counter"
+    },
+    "corpor": {
+        ">>>": "body"
+    },
+    "cost(o)": {
+        ">>>": "rib"
+    },
+    "crani(o)": {
+        ">>>": "skull"
+    },
+    "cry(o)": {
+        ">>>": "cold"
+    },
+    "cut": {
+        ">>>": "skin"
+    },
+    "cyan(o)": {
+        ">>>": "blue"
+    },
+    "cyst(o)": {
+        ">>>": "bladder"
+    },
+    "cyt(o)": {
+        ">>>": "cell"
+    },
+    "dactyl(o)": {
+        ">>>": "finger or toe"
+    },
+    "dent": {
+        ">>>": "tooth"
+    },
+    "derm(ato)": {
+        ">>>": "skin"
+    },
+    "dipl(o)": {
+        ">>>": "double"
+    },
+    "dors": {
+        ">>>": "back"
+    },
+    "dys": {
+        ">>>": "bad, faulty, abnormal"
+    },
+    "ectomy": {
+        ">>>": "excision (removal by cutting)"
+    },
+    "emia": {
+        ">>>": "blood"
+    },
+    "encephal(o)": {
+        ">>>": "brain"
+    },
+    "end(o)": {
+        ">>>": "inside"
+    },
+    "enter(o)": {
+        ">>>": "intestine"
+    },
+    "epi": {
+        ">>>": "outer, superficial, upon"
+    },
+    "erythr(o)": {
+        ">>>": "red"
+    },
+    "eu": {
+        ">>>": "normal"
+    },
+    "extra": {
+        ">>>": "outside"
+    },
+    "gastr(o)": {
+        ">>>": "stomach"
+    },
+    "gen": {
+        ">>>": "become, originate"
+    },
+    "gloss(o)": {
+        ">>>": "tongue"
+    },
+    "glyc(o)": {
+        ">>>": "sweet, or referring to glucose"
+    },
+    "gram, graph": {
+        ">>>": "write, record"
+    },
+    "gyn": {
+        ">>>": "woman"
+    },
+    "hem(ato)": {
+        ">>>": "blood"
+    },
+    "hemi": {
+        ">>>": "half"
+    },
+    "hepat(o)": {
+        ">>>": "liver"
+    },
+    "hist(o)": {
+        ">>>": "tissue"
+    },
+    "hydr(o)": {
+        ">>>": "water"
+    },
+    "hyper": {
+        ">>>": "excessive, high"
+    },
+    "hypo": {
+        ">>>": "deficient, low"
+    },
+    "hyster(o)": {
+        ">>>": "uterus"
+    },
+    "iatr(o)": {
+        ">>>": "doctor"
+    },
+    "infra": {
+        ">>>": "beneath"
+    },
+    "inter": {
+        ">>>": "among, between"
+    },
+    "intra": {
+        ">>>": "inside"
+    },
+    "itis": {
+        ">>>": "inflammation"
+    },
+    "lact(o)": {
+        ">>>": "milk"
+    },
+    "lapar(o)": {
+        ">>>": "flank, abdomen"
+    },
+    "latero": {
+        ">>>": "side"
+    },
+    "leuk(o)": {
+        ">>>": "white"
+    },
+    "lingu(o)": {
+        ">>>": "tongue"
+    },
+    "lip(o)": {
+        ">>>": "fat"
+    },
+    "lys(is)": {
+        ">>>": "dissolve"
+    },
+    "mal": {
+        ">>>": "bad, abnormal"
+    },
+    "malac": {
+        ">>>": "soft"
+    },
+    "mamm(o)": {
+        ">>>": "breast"
+    },
+    "mast(o)": {
+        ">>>": "breast"
+    },
+    "megal(o)": {
+        ">>>": "large"
+    },
+    "melan(o)": {
+        ">>>": "black"
+    },
+    "mening(o)": {
+        ">>>": "membranes"
+    },
+    "my(o)": {
+        ">>>": "muscle"
+    },
+    "myc(o)": {
+        ">>>": "fungus"
+    },
+    "myel(o)": {
+        ">>>": "marrow"
+    },
+    "nas(o)": {
+        ">>>": "nose"
+    },
+    "necr(o)": {
+        ">>>": "death"
+    },
+    "nephr(o)": {
+        ">>>": "kidney"
+    },
+    "neur(o)": {
+        ">>>": "nerve"
+    },
+    "nutri": {
+        ">>>": "nourish"
+    },
+    "ocul(o)": {
+        ">>>": "eye"
+    },
+    "odyn(o)": {
+        ">>>": "pain"
+    },
+    "oma": {
+        ">>>": "tumor"
+    },
+    "onc(o)": {
+        ">>>": "tumor"
+    },
+    "oophor(o)": {
+        ">>>": "ovaries"
+    },
+    "ophthalm(o)": {
+        ">>>": "eye"
+    },
+    "opia": {
+        ">>>": "vision"
+    },
+    "opsy": {
+        ">>>": "examination"
+    },
+    "orchi(o)": {
+        ">>>": "testes"
+    },
+    "osis": {
+        ">>>": "condition"
+    },
+    "osse(o)": {
+        ">>>": "bone"
+    },
+    "oste(o)": {
+        ">>>": "bone"
+    },
+    "ot(o)": {
+        ">>>": "ear"
+    },
+    "path(o)": {
+        ">>>": "disease"
+    },
+    "ped(o)": {
+        ">>>": "child"
+    },
+    "penia": {
+        ">>>": "deficient, deficiency"
+    },
+    "peps, pept": {
+        ">>>": "digest"
+    },
+    "peri": {
+        ">>>": "around"
+    },
+    "phag(o)": {
+        ">>>": "eat, destroy"
+    },
+    "pharmaco": {
+        ">>>": "drug"
+    },
+    "pharyng(o)": {
+        ">>>": "throat"
+    },
+    "phleb(o)": {
+        ">>>": "vein"
+    },
+    "phob(ia)": {
+        ">>>": "fear"
+    },
+    "plasty": {
+        ">>>": "repair"
+    },
+    "pleg(ia)": {
+        ">>>": "paralysis"
+    },
+    "pnea": {
+        ">>>": "breathing"
+    },
+    "pneum(ato)": {
+        ">>>": "breath, air"
+    },
+    "pneumon(o)": {
+        ">>>": "lung"
+    },
+    "pod(o)": {
+        ">>>": "foot"
+    },
+    "poie": {
+        ">>>": "make, produce"
+    },
+    "poly": {
+        ">>>": "much, many"
+    },
+    "post": {
+        ">>>": "after"
+    },
+    "poster(i)": {
+        ">>>": "back, behind"
+    },
+    "presby": {
+        ">>>": "elder"
+    },
+    "proct(o)": {
+        ">>>": "anus"
+    },
+    "pseud(o)": {
+        ">>>": "false"
+    },
+    "psych(o)": {
+        ">>>": "mind"
+    },
+    "pulmon(o)": {
+        ">>>": "lung"
+    },
+    "pyel(o)": {
+        ">>>": "pelvis of kidney"
+    },
+    "pyr(o)": {
+        ">>>": "fever, fire"
+    },
+    "rachi(o)": {
+        ">>>": "spine"
+    },
+    "ren(o)": {
+        ">>>": "kidneys"
+    },
+    "rhag": {
+        ">>>": "break, burst"
+    },
+    "rhe": {
+        ">>>": "flow"
+    },
+    "rhin(o)": {
+        ">>>": "nose"
+    },
+    "scler(o)": {
+        ">>>": "hard"
+    },
+    "scope": {
+        ">>>": "instrument"
+    },
+    "scopy": {
+        ">>>": "examination"
+    },
+    "somat(o)": {
+        ">>>": "body"
+    },
+    "spondyl(o)": {
+        ">>>": "vertebra"
+    },
+    "steat(o)": {
+        ">>>": "fat"
+    },
+    "sten(o)": {
+        ">>>": "narrow, comopressed"
+    },
+    "steth(o)": {
+        ">>>": "chest"
+    },
+    "stom": {
+        ">>>": "mouth, opening"
+    },
+    "supra": {
+        ">>>": "above"
+    },
+    "tachy": {
+        ">>>": "fast, quick"
+    },
+    "therap": {
+        ">>>": "treatment"
+    },
+    "therm(o)": {
+        ">>>": "heat"
+    },
+    "thorac(o)": {
+        ">>>": "chest"
+    },
+    "thromb(o)": {
+        ">>>": "clot, lump"
+    },
+    "tomy": {
+        ">>>": "incision (operation by cutting)"
+    },
+    "toxi": {
+        ">>>": "poison"
+    },
+    "uria": {
+        ">>>": "urine"
+    },
+    "vas(o)": {
+        ">>>": "vessel"
+    },
+    "ven(o)": {
+        ">>>": "vein"
+    },
+    "vesic(o)": {
+        ">>>": "bladder"
+    },
+    "xer(o)": {
+        ">>>": "dry"
+    },
+
+}
+
+// https://stackoverflow.com/a/3719011
+function randomize(a, b) {
+    return Math.random() - 0.5;
+}
+
+var ijo = Object.keys(terms);
+ijo.sort(randomize);
+
+var i = 0;
+
+function processClick() {    
+    var toki = ijo[i++]
+    var ilo = terms[toki];
+    $("#toki").html
+    (   "<div class='text-center' style='font-size:16px;'>" + i + "/120</div><hr>" +
+        "<div class='text-center' style='font-size:64px;'><strong>" + toki + "</strong></div><hr>"
+        );
+    // var def = "<ul class='list-group'>";
+    var def = "";
+    for(var x in ilo) {
+        def += "<div class='text-center' style='font-size:32px;'><strong>" + ilo[x] + "</strong></div><hr>"
+        // def += "<li class='list-group-item'><em>" + x + ".</em> " + ilo[x] + "</li>";
+    }
+    // def += "</ul>"
+    $("#def").html(def);
+
+    if(i == 120) {
+        i = 0;
+    }
+}
